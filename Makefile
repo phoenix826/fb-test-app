a5ada38ceae08a34b55155c6d2e610b542dfdc0c
Tomi Valkeinen <tomi.valkeinen@nokia.com>
2009-12-02 15:04:15 +0200

    add offset and pan tests

--- Makefile
+++ Makefile
@@ -1,8 +1,9 @@
 
 CC=$(CROSS_COMPILE)gcc
 CFLAGS=-I/home/valkeine/work/linux/usr/include -O2 -Wall
+LDFLAGS=-lm
 
-all: db readback upd perf rect test offset
+all: db readback upd perf rect test offset pan
 
 clean:
-	rm -f db readback upd perf rect test offset
+	rm -f db readback upd perf rect test offset pan
---
+++ offset.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <time.h>
+
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+
+#include <linux/fb.h>
+#include <linux/omapfb.h>
+
+#define ERROR(x) printf("fbtest error in line %s:%d: %s\n", __FUNCTION__, __LINE__, strerror(errno));
+
+#define FBCTL(cmd, arg)			\
+	if(ioctl(fd, cmd, arg) == -1) {	\
+		ERROR("ioctl failed");	\
+		exit(1); }
+
+#define FBCTL0(cmd)			\
+	if(ioctl(fd, cmd) == -1) {	\
+		ERROR("ioctl failed");	\
+		exit(1); }
+
+
+int main(int argc, char** argv)
+{
+	char str[64];
+	int fd;
+	struct fb_var_screeninfo var;
+
+	int opt;
+	int req_fb = 0;
+
+	while ((opt = getopt(argc, argv, "f:")) != -1) {
+		switch (opt) {
+		case 'f':
+			req_fb = atoi(optarg);
+			break;
+		default:
+			printf("usage: -f <fbnum> -r <rot> -m <bitspp> "
+					"-y <yuv>\n");
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	if (argc - optind != 2) {
+		printf("give x and y offsets\n");
+		return -1;
+	}
+
+	sprintf(str, "/dev/fb%d", req_fb);
+	fd = open(str, O_RDWR);
+
+	FBCTL(FBIOGET_VSCREENINFO, &var);
+	var.xoffset = atoi(argv[optind + 0]);
+	var.yoffset = atoi(argv[optind + 1]);
+	FBCTL(FBIOPUT_VSCREENINFO, &var);
+
+	return 0;
+}
