755779c86575ff5f3e5f46738303ed231bf055cb
Tomi Valkeinen <tomi.valkeinen@nokia.com>
2010-04-20 14:42:03 +0300

    ovl improvement

--- ovl.c
+++ ovl.c
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <getopt.h>
 #include <time.h>
+#include <math.h>
 
 #include <sys/ioctl.h>
 #include <sys/stat.h>
@@ -111,9 +112,39 @@ static void fill_screen(struct fb_info *fb_info)
 	}
 }
 
+void move_ovl(unsigned x, unsigned y, unsigned w, unsigned h,
+		unsigned ox, unsigned oy)
+{
+	int fd = fb_info.fd;
+	struct fb_var_screeninfo *var = &fb_info.var;
+	struct omapfb_plane_info pi;
+
+	printf("%d,%d %dx%d (%d,%d)\n", x, y, w, h, ox, oy);
+
+
+	IOCTL1(fd, OMAPFB_QUERY_PLANE, &pi);
+	pi.enabled = 0;
+	IOCTL1(fd, OMAPFB_SETUP_PLANE, &pi);
+
+	IOCTL1(fd, FBIOGET_VSCREENINFO, var);
+	var->xres = w;
+	var->yres = h;
+	var->xoffset = ox;
+	var->yoffset = oy;
+	IOCTL1(fd, FBIOPUT_VSCREENINFO, var);
+
+	IOCTL1(fd, OMAPFB_QUERY_PLANE, &pi);
+	pi.pos_x = x;
+	pi.pos_y = y;
+	pi.enabled = 1;
+	IOCTL1(fd, OMAPFB_SETUP_PLANE, &pi);
+
+}
+
 int main(int argc, char** argv)
 {
 	int fb_num;
+	unsigned c;
 
 	if (argc == 2)
 		fb_num = atoi(argv[1]);
@@ -124,10 +155,36 @@ int main(int argc, char** argv)
 
 	fill_screen(&fb_info);
 
-	if (fb_info.update_mode == OMAPFB_MANUAL_UPDATE) {
-		fb_update_window(fb_info.fd, 0, 0,
-				fb_info.di.xres, fb_info.di.yres);
-		fb_sync_gfx(fb_info.fd);
+	c = 0;
+	while (1) {
+		unsigned x, y, w, h, ox, oy;
+
+		w = (sin((float)c * M_PI / 180 - M_PI / 2) + 1.0) / 2.0 *
+			400 + 8;
+
+		h = (sin((float)c * M_PI / 180 *1.2 - M_PI / 2) + 1.0) / 2.0 *
+			400 + 8;
+
+		x = (sin((float)c * M_PI / 180 - M_PI / 2) + 1.0) / 2.0 *
+			(fb_info.di.xres - w - 1);
+
+		y = (sin((float)c * M_PI / 180 * 1.3 - M_PI / 2) + 1.0) / 2.0 *
+			(fb_info.di.yres - h - 1);
+
+		ox = (cos((float)c * M_PI / 180 * 1.1 - M_PI / 2) + 1.0) / 2.0 *
+			(fb_info.var.xres_virtual - w);
+
+		oy = (sin((float)c * M_PI / 180 * 1.2 - M_PI / 2) + 1.0) / 2.0 *
+			(fb_info.var.yres_virtual - h);
+
+		move_ovl(x, y, w, h, ox, oy);
+
+		if (fb_info.update_mode == OMAPFB_MANUAL_UPDATE) {
+			fb_update_window(fb_info.fd, 0, 0,
+					fb_info.di.xres, fb_info.di.yres);
+			fb_sync_gfx(fb_info.fd);
+		}
+		c++;
 	}
 
 	return 0;
