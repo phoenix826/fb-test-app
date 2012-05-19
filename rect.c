e7a3515d6715ae6255e0f0a85485a7a3aeb4a68e
David Anders <danders.dev@gmail.com>
2012-05-19 10:18:37 -0500

    code cleanup to pass checkpatch

--- rect.c
+++ rect.c
@@ -32,27 +32,27 @@
 
 #include <linux/fb.h>
 
-#define ERROR(x) printf("fbtest error in line %s:%d: %s\n", __FUNCTION__, __LINE__, strerror(errno));
+#define ERROR(x) printf("fbtest error in line %s:%d: %s\n", __func__, \
+	__LINE__, strerror(errno));
 
-#define FBCTL(cmd, arg)			\
-	if(ioctl(fd, cmd, arg) == -1) {	\
-		ERROR("ioctl failed");	\
-		exit(1); }
+#define FBCTL(cmd, arg)				\
+	(if (ioctl(fd, cmd, arg) == -1) {	\
+		ERROR("ioctl failed");		\
+		exit(1); })
 
-#define FBCTL0(cmd)			\
-	if(ioctl(fd, cmd) == -1) {	\
-		ERROR("ioctl failed");	\
-		exit(1); }
+#define FBCTL0(cmd)				\
+	(if (ioctl(fd, cmd) == -1) {		\
+		ERROR("ioctl failed");		\
+		exit(1); })
 
 static struct fb_var_screeninfo var;
 static struct fb_fix_screeninfo fix;
 
-int open_fb(const char* dev)
+int open_fb(const char *dev)
 {
 	int fd = -1;
 	fd = open(dev, O_RDWR);
-	if(fd == -1)
-	{
+	if (fd == -1) {
 		printf("Error opening device %s : %s\n", dev, strerror(errno));
 		exit(-1);
 	}
@@ -72,17 +72,17 @@ static struct rect *get_rand_rect(struct rect *r,
 		short min_w, short min_h,
 		short max_w, short max_h)
 {
-#define MIN(x,y) ( ((x) < (y)) ? (x) : (y) )
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
 
 	const short max_width = MIN(max_w, max_x)+1;
 	const short max_height = MIN(max_h, max_y)+1;
 
-	if(min_w == max_x)
+	if (min_w == max_x)
 		r->x = 0;
 	else
 		r->x = rand() % (max_x - min_w);
 
-	if(min_h == max_y)
+	if (min_h == max_y)
 		r->y = 0;
 	else
 		r->y = rand() % (max_y - min_h);
@@ -90,7 +90,11 @@ static struct rect *get_rand_rect(struct rect *r,
 	r->w = min_w + rand() % (MIN(max_width, 1+max_x - r->x - min_w));
 	r->h = min_h + rand() % (MIN(max_height, 1+max_y - r->y -  min_h));
 
-	//	printf("Returning x = %d, y = %d, w = %d, h = %d\n", r->x, r->y, r->w, r->h);
+#if 0
+	printf("Returning x = %d, y = %d, w = %d, h = %d\n",
+		r->x, r->y, r->w, r->h);
+#endif
+
 	return r;
 }
 
@@ -131,19 +135,19 @@ static void draw_pixel(void *fbmem, int x, int y, unsigned int color)
 
 static unsigned fill_rect(int *fb, const struct rect *r)
 {
-	short x,y;
+	short x, y;
 
 	int color = rand() % (0xffffff+1);
 	const int max_w = r->x + r->w;
 	const int max_h = r->y + r->h;
 
-	//printf("fill_rect %d,%d %dx%d\n", r->x, r->y, r->w, r->h);
+#if 0
+	printf("fill_rect %d,%d %dx%d\n", r->x, r->y, r->w, r->h);
+#endif
 
-	for(y = r->y; y < max_h; y++)
-	{
-		for(x = r->x; x < max_w; x++)
-		{
-			if(y - r->y == x - r->x)
+	for (y = r->y; y < max_h; y++) {
+		for (x = r->x; x < max_w; x++) {
+			if (y - r->y == x - r->x)
 				draw_pixel(fb, x, y, ~color & 0xffffff);
 			else
 				draw_pixel(fb, x, y, color);
@@ -175,7 +179,7 @@ void fill_screen(void *fbmem)
 	}
 }
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
 	int fd;
 	struct rect r;
@@ -194,11 +198,11 @@ int main(int argc, char** argv)
 	readbuf_size = var.xres_virtual * var.yres_virtual * 3;
 	readbuf = malloc(readbuf_size);
 
-	void* ptr = mmap(0, var.yres_virtual * fix.line_length,
+	void *ptr = mmap(0, var.yres_virtual * fix.line_length,
 			PROT_WRITE | PROT_READ,
 			MAP_SHARED, fd, 0);
 
-	if(ptr == MAP_FAILED) {
+	if (ptr == MAP_FAILED) {
 		perror("mmap failed");
 		exit(1);
 	}
